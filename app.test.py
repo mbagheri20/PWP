e9a5bcef2bbe7fe0fb9ef27cfa80dd858b00ed17
JoniBarsk <joni.barsk@outlook.com>
2021-02-26 16:52:47 +0200

    better json.loads

--- app.test.py
+++ app.test.py
@@ -2,20 +2,10 @@ import unittest
 from app import app
 from mongoengine import connect, disconnect
 import json
+from flask import Flask, request, jsonify
 
 
 class BasicTestCase(unittest.TestCase):
-    
-    longMessage = True
-
-    @classmethod
-    def setUpClass(cls):
-        connect('mongoenginetest', host='mongomock://localhost', alias='testdb')
-
-    @classmethod
-    def tearDownClass(cls):
-       disconnect()
-
     def test_home(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
@@ -26,18 +16,13 @@ class BasicTestCase(unittest.TestCase):
     def test_get_material(self):
         tester = app.test_client(self)
         response = tester.get('/material/', content_type='application/json')
-        print("setting up", response.data.decode('UTF-8'))
+        resp =  json.loads(response.data)
+        comparison_data =  [{"id":"603773b204b4a0c9781eb187","structure name":"H"},{"id":"603773b704b4a0c9781eb188","structure name":"He"}]
+        print("response up", resp)
+        print("comparison_data up", comparison_data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('UTF-8'), 
-            [{
-                "id":"603773b204b4a0c9781eb187",
-                "structure name" : "H"
-            },{
-                "id":"603773b704b4a0c9781eb188",
-                "structure name" : "He"
-            }]
-        )
-        
+        self.assertEqual(resp, comparison_data)
+
     # def test_other(self):
     #         tester = app.test_client(self)
     #         response = tester.get('a', content_type='html/text')
