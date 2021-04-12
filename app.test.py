dda697ca5b47826be2f04bf163a8ec6af36e63e5
JoniBarsk <joni.barsk@outlook.com>
2021-04-12 15:00:32 +0300

    coverage and app and test fixes

--- app.test.py
+++ app.test.py
@@ -10,17 +10,18 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello World!')
-        
-        
+        self.assertEqual(response.data, b'Try /api/')
+
     def test_get_material(self):
         tester = app.test_client(self)
-        response = tester.get('/api/material/', content_type='application/json')
-        resp =  json.loads(response.data)
-        comparison_data =  [{"id":"603773b204b4a0c9781eb187","structure name":"H"},{"id":"603773b704b4a0c9781eb188","structure name":"He"}]
+        response = tester.get(
+            '/api/material/', content_type='application/json')
+        resp = json.loads(response.data)
+        comparison_data = {'@namespaces': {'material_db': {'name': '/material/link-relations/'}}, '@controls': {'self': {'href': '/api/material/'}, '${MATERIAL_DB}:material-all': {'method': 'GET', 'title': 'Get all material objects', 'href': '/api/material/'}, '${MATERIAL_DB}:add-material': {'method': 'POST', 'encoding': 'json', 'title': 'Add a new material', 'schema': {'type': 'object', 'required': ['structure_name'], 'properties': {'structure_name': {
+            'description': "Material's structure name", 'type': 'string'}}}, 'href': '/api/material/'}}, 'items': [{'structure_name': 'a', '@controls': {'self': {'href': '/api/material/a/'}, 'profile': {'href': '/profiles/material/'}}}, {'structure_name': 'b', '@controls': {'self': {'href': '/api/material/b/'}, 'profile': {'href': '/profiles/material/'}}}, {'structure_name': 'c', '@controls': {'self': {'href': '/api/material/c/'}, 'profile': {'href': '/profiles/material/'}}}]}
         print("response up", resp)
         print("comparison_data up", comparison_data)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(resp, comparison_data)
 
     # def test_other(self):
