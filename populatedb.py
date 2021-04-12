71499caeba862b56fc026a177ddde5f5965ed9b7
JoniBarsk <joni.barsk@outlook.com>
2021-04-12 14:35:08 +0300

    better population method for mongo

---
+++ populatedb.py
@@ -0,0 +1,138 @@
+import json
+from flask import Flask, request
+from flask.wrappers import Response
+from flask_restful import Api, Resource, abort
+from flask_mongoengine import MongoEngine
+from mongoengine.connection import connect
+from mongoengine.errors import ValidationError
+from app import Material, Material_Fermi, Material_Volume, error, app
+
+populate_db = Flask(__name__)
+populate_db.config['MONGODB_SETTINGS'] = {
+    'db': 'your_database',
+    'host': 'mongodb://localhost/db',
+    'port': 27017
+}
+db = MongoEngine(populate_db)
+
+class Migration(db.Document):  # class for migration
+    migration = db.StringField(required=True, unique=True, max_length=64)
+    successful = db.BooleanField(required=True)
+
+    def to_json(self):
+        if self.id is not None:
+            return {"id": str(self.id), "migration": self.migration}
+        return {"migration": self.migration}
+
+
+def initialize():
+    migrations = []
+    material_migration = ("Material", update_values(material(), "Material"))
+    migrations.append(material_migration)
+    volume_migration = ("Material_Volume", update_values(volume(), "Material_Volume"))
+    migrations.append(volume_migration)
+    fermi_migration = ("Material_Fermi", update_values(fermi(), "Material_Fermi"))
+    migrations.append(fermi_migration)
+    for each in migrations:
+        if each:
+            mig = Migration(migration=each[0], successful=each[1])
+            mig.save()
+            print("migration updated")
+        else: 
+            mig = Migration(migration=each[0], successful=each[1])
+            mig.save()
+            print("something went wront :(")
+    return
+
+def update_values(dictlist: list, type: any) -> bool:
+    classTypeEnum
+    if type not in classTypeEnum.keys():
+        print("error")
+        return False
+    try:
+        for each in dictlist:
+            thign = classTypeEnum[type](**each)
+            thign.save()
+            print(str(classTypeEnum[type]) + " saved") 
+        return True
+    except ValueError:
+        print("error occured")
+        return False
+
+def material() -> list:
+    """return list of dicts
+    [{material1}, {material2}]
+    """
+    material_list = [
+        {"structure_name": "a"},
+        {"structure_name": "b"},
+        {"structure_name": "c"}
+    ]
+    return material_list
+
+def volume() -> list:
+    """return list of dicts
+    [{volume1}, {volume2}, {volume3}]
+    """
+    materials = Material.objects().all()
+    volume_list = [
+        {
+            "size_a" : 1.1,
+            "size_b" : 1.11,
+            "size_c" : 1.111,
+            "bonding_length" : 1,
+            "dimension_type" : "3d",
+            "material" : materials[0].id
+        },
+        {
+            "size_a" : 1.2,
+            "size_b" : 1.22,
+            "size_c" : 1.222,
+            "bonding_length" : 2,
+            "dimension_type" : "3d",
+            "material" : materials[1].id
+        },
+        {
+            "size_a" : 1.3,
+            "size_b" : 1.33,
+            "bonding_length" : 3,
+            "dimension_type" : "2d",
+            "material" : materials[2].id
+        },
+    ]
+    return volume_list
+
+def fermi() -> list:
+    """return list of dicts
+    [{fermi}, {fermi}, {fermi}]
+    """
+    fermi_list = []
+    volumes = Material_Volume.objects().all()
+    for each_volume in volumes:
+        fermi = {"volume": each_volume.id,
+                 "material": each_volume.material.id,
+                 "fermi" : 0.42
+                 }
+        print(each_volume.to_json())
+        fermi_list.append(fermi)
+    return fermi_list
+
+def found_migrations()-> bool:
+    migration = Migration.objects().first()
+    if migration is None:
+        return False
+    return True
+
+classTypeEnum = {
+    "Material": Material, 
+    "Material_Volume": Material_Volume, 
+    "Material_Fermi": Material_Fermi
+}
+
+if __name__ == '__main__':
+    if not found_migrations():
+        initialize()
+    else:
+        print("Migrations Found")
+    exit()
+
