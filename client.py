f222347c08939f9f832fd06f066e7414c5e82cb1
Worpaaja <lasse_780@hotmail.com>
2021-04-23 09:16:06 +0300

    added ikkunasto.py
    Now with all the libraries

--- client.py
+++ client.py
@@ -157,6 +157,45 @@ def send_put_material_fermi():
         pass
     ui.kirjoita_tekstilaatikkoon(state["text box"], response, True) #print print
 
+def delete_material():
+    state["nappi"].destroy()
+    state["write box"] = None
+    ui.kirjoita_tekstilaatikkoon(state["text box"], "Enter material name for delete", True)
+    state["nappi"] = ui.luo_nappi(state["oikea"], "Submit", send_delete_material)
+
+def send_delete_material():
+    try:
+        response = "a response"
+    except:
+        pass
+    ui.kirjoita_tekstilaatikkoon(state["text box"], response, True) #print print
+
+def delete_material_volume():
+    state["nappi"].destroy()
+    state["write box"] = None
+    ui.kirjoita_tekstilaatikkoon(state["text box"], "Enter material volume id for delete", True)
+    state["nappi"] = ui.luo_nappi(state["oikea"], "Submit", send_delete_material_volume)
+
+def send_delete_material_volume():
+    try:
+        response = "a response"
+    except:
+        pass
+    ui.kirjoita_tekstilaatikkoon(state["text box"], response, True) #print print
+
+def delete_material_fermi():
+    state["nappi"].destroy()
+    state["write box"] = None
+    ui.kirjoita_tekstilaatikkoon(state["text box"], "Enter material fermi id for delete", True)
+    state["nappi"] = ui.luo_nappi(state["oikea"], "Submit", send_delete_material_fermi)
+
+def send_delete_material_fermi():
+    try:
+        response = "a response"
+    except:
+        pass
+    ui.kirjoita_tekstilaatikkoon(state["text box"], response, True) #print print
+
 def empty():
     pass
 
@@ -173,6 +212,9 @@ def main():
     ui.luo_nappi(state["vasen"], "Edit a material", edit_material)
     ui.luo_nappi(state["vasen"], "Edit a material volume", edit_material_volume)
     ui.luo_nappi(state["vasen"], "Edit a material fermi", edit_material_fermi)
+    ui.luo_nappi(state["vasen"], "Delete a material", delete_material)
+    ui.luo_nappi(state["vasen"], "Delete a material volume", delete_material_volume)
+    ui.luo_nappi(state["vasen"], "Delete a material fermi", delete_material_fermi)
     ui.luo_nappi(state["vasen"], "Quit", ui.lopeta)
     state["text box"] = ui.luo_tekstilaatikko(state["oikea"], 50,30)
     state["write box"] = ui.luo_tekstikentta(state["oikea"])
